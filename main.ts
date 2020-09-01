a2262f679493b81312ecef02bbdfe2797e33f788
Kittanan <57888441+KIttanan@users.noreply.github.com>
2020-09-01 13:59:39 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,24 @@
 /**
-     * Turns left by 90 degrees
-     */
-    //% blockId=turtleTurnLeft block="turn left"
+ * BearLab graphics blocks
+ */
+//% weight=100 color=#0f9c11 icon="\f1b0"
+namespace BearLab {
+
+    /**
+         * Turns left by 90 degrees
+         */
+    //% blockId=BearLabTurnLeft block="turn left"
     //% weight=89 blockGap=8
-input.onButtonPressed(Button.A, function () {
-    led.toggle(0, 0)
-})
-/**
-     * Turns left by 90 degrees
-     */
-    //% blockId=turtleTurnRight block="turn right"
+    input.onButtonPressed(Button.A, function () {
+        led.toggle(0, 0)
+    })
+    /**
+         * Turns left by 90 degrees
+         */
+    //% blockId=BearLabTurnRight block="turn right"
     //% weight=89 blockGap=8
-input.onButtonPressed(Button.B, function () {
-    led.toggle(0, 0)
-})
+    input.onButtonPressed(Button.B, function () {
+        led.toggle(0, 0)
+    })
+    
+}
