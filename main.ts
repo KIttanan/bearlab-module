f90b153ebfda59196d28d10d50fe012c7774c7ec
Kittanan <57888441+KIttanan@users.noreply.github.com>
2020-09-01 14:44:15 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 /**
  * BearLab graphics blocks
  */
-//% color=190 weight=100 icon="\f1b0" block="BearLab Blocks"
+//% color=190 weight=100 icon="\f1b0" block="BearLab"
 //% groups=['LED matrix', 'Control flow', 'others']
 namespace BearLab {
 
     //% blockId=device_show_number
     //% block="show|number %v"
-    export function showNumber(v: number, interval: number = 150): void { }
+    export function showNumber0(v: number, interval: number = 150): void { }
 
     //% block
     export function showNumber1(v: number, interval: number = 150): void { }
