1806d71811770da3bb0840adf42920c6aaf97b16
Kittanan <57888441+KIttanan@users.noreply.github.com>
2020-09-01 13:57:05 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
+/**
+     * Turns left by 90 degrees
+     */
+    //% blockId=turtleTurnLeft block="turn left"
+    //% weight=89 blockGap=8
 input.onButtonPressed(Button.A, function () {
     led.toggle(0, 0)
 })
+/**
+     * Turns left by 90 degrees
+     */
+    //% blockId=turtleTurnRight block="turn right"
+    //% weight=89 blockGap=8
 input.onButtonPressed(Button.B, function () {
     led.toggle(0, 0)
 })
